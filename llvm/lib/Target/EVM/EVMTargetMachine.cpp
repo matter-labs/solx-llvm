Lubov66/era-compiler-llvm

[EVM] Add implementation of AliasAnalysis
Signed-off-by: Vladimir Radosavljevic <vr@matterlabs.dev>

llvm/lib/Target/EVM/EVMTargetMachine.cpp
@@ -12,6 +12,7 @@
 
 #include "EVM.h"
 
+#include "EVMAliasAnalysis.h"
 #include "EVMMachineFunctionInfo.h"
 #include "EVMTargetMachine.h"
 #include "EVMTargetObjectFile.h"
@@ -58,6 +59,8 @@ extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeEVMTarget() {
   initializeEVMSplitCriticalEdgesPass(PR);
   initializeEVMStackifyPass(PR);
   initializeEVMBPStackificationPass(PR);
+  initializeEVMAAWrapperPassPass(PR);
+  initializeEVMExternalAAWrapperPass(PR);
 }
 
 static std::string computeDataLayout() {
@@ -114,6 +117,10 @@ bool EVMTargetMachine::parseMachineFunctionInfo(
   return false;
 }
 
+void EVMTargetMachine::registerDefaultAliasAnalyses(AAManager &AAM) {
+  AAM.registerFunctionAnalysis<EVMAA>();
+}
+
 void EVMTargetMachine::registerPassBuilderCallbacks(PassBuilder &PB) {
   PB.registerPipelineStartEPCallback(
       [](ModulePassManager &PM, OptimizationLevel Level) {
@@ -126,6 +133,18 @@ void EVMTargetMachine::registerPassBuilderCallbacks(PassBuilder &PB) {
         if (Level.getSizeLevel() || Level.getSpeedupLevel() > 1)
           PM.addPass(MergeIdenticalBBPass());
       });
+
+  PB.registerAnalysisRegistrationCallback([](FunctionAnalysisManager &FAM) {
+    FAM.registerPass([] { return EVMAA(); });
+  });
+
+  PB.registerParseAACallback([](StringRef AAName, AAManager &AAM) {
+    if (AAName == "evm-aa") {
+      AAM.registerFunctionAnalysis<EVMAA>();
+      return true;
+    }
+    return false;
+  });
 }
 
 namespace {
@@ -160,6 +179,14 @@ public:
 
 void EVMPassConfig::addIRPasses() {
   addPass(createEVMLowerIntrinsicsPass());
+  if (TM->getOptLevel() != CodeGenOpt::None) {
+    addPass(createEVMAAWrapperPass());
+    addPass(
+        createExternalAAWrapperPass([](Pass &P, Function &, AAResults &AAR) {
+          if (auto *WrapperPass = P.getAnalysisIfAvailable<EVMAAWrapperPass>())
+            AAR.addAAResult(WrapperPass->getResult());
+        }));
+  }
   TargetPassConfig::addIRPasses();
 }
 

matter-labs/solx-llvm

llvm/lib/Target/EVM/EVMTargetMachine.cpp
@@ -11,6 +11,8 @@
 //===----------------------------------------------------------------------===//
 
 #include "EVM.h"
+
+#include "EVMAliasAnalysis.h"
 #include "EVMMachineFunctionInfo.h"
 #include "EVMTargetMachine.h"
 #include "EVMTargetObjectFile.h"
@@ -50,6 +52,8 @@ extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeEVMTarget() {
   initializeEVMSingleUseExpressionPass(PR);
   initializeEVMSplitCriticalEdgesPass(PR);
   initializeEVMBPStackificationPass(PR);
+  initializeEVMAAWrapperPassPass(PR);
+  initializeEVMExternalAAWrapperPass(PR);
 }
 
 static std::string computeDataLayout() {
@@ -106,6 +110,10 @@ bool EVMTargetMachine::parseMachineFunctionInfo(
   return false;
 }
 
+void EVMTargetMachine::registerDefaultAliasAnalyses(AAManager &AAM) {
+  AAM.registerFunctionAnalysis<EVMAA>();
+}
+
 void EVMTargetMachine::registerPassBuilderCallbacks(PassBuilder &PB) {
   PB.registerPipelineStartEPCallback(
       [](ModulePassManager &PM, OptimizationLevel Level) {
@@ -118,6 +126,18 @@ void EVMTargetMachine::registerPassBuilderCallbacks(PassBuilder &PB) {
         if (Level.getSizeLevel() || Level.getSpeedupLevel() > 1)
           PM.addPass(MergeIdenticalBBPass());
       });
+
+  PB.registerAnalysisRegistrationCallback([](FunctionAnalysisManager &FAM) {
+    FAM.registerPass([] { return EVMAA(); });
+  });
+
+  PB.registerParseAACallback([](StringRef AAName, AAManager &AAM) {
+    if (AAName == "evm-aa") {
+      AAM.registerFunctionAnalysis<EVMAA>();
+      return true;
+    }
+    return false;
+  });
 }
 
 namespace {
@@ -152,6 +172,14 @@ public:
 
 void EVMPassConfig::addIRPasses() {
   addPass(createEVMLowerIntrinsicsPass());
+  if (TM->getOptLevel() != CodeGenOptLevel::None) {
+    addPass(createEVMAAWrapperPass());
+    addPass(
+        createExternalAAWrapperPass([](Pass &P, Function &, AAResults &AAR) {
+          if (auto *WrapperPass = P.getAnalysisIfAvailable<EVMAAWrapperPass>())
+            AAR.addAAResult(WrapperPass->getResult());
+        }));
+  }
   TargetPassConfig::addIRPasses();
 }
 